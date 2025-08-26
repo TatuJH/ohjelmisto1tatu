e5077a104195a1a00c871ad426f0c49930fd7154
TatuJH <tatu.helminen@metropolia.fi>
2025-08-26 14:10:14 +0300

    tehtävä 5

---
+++ mod5/tehtava5.py
@@ -0,0 +1,42 @@
+import random
+
+#Tehtävä 5.1
+nopat = int(input("Monta arpakuutiota heitetään?"))
+summa = 0
+for i in range(0,nopat):
+    summa += random.randint(1,6)
+    i += 1
+print(f"Arpakuutioiden summa on {summa}.")
+
+#Tehtävä 5.2
+luvut = []
+luku = input("Anna jokin luku")
+while luku != "":
+    luku = int(luku)
+    luvut.append(luku)
+    luku = input("Anna jokin luku tai lopeta enterillä")
+luvut.sort(reverse=True)
+for i in range(0,5):
+    print(luvut[i])
+
+#Tehtävä 5.3
+luku = int(input("Anna jokin kokonaisluku"))
+alkuluku = True
+if luku >= 2:
+    for i in range(2,luku):
+        if luku % i == 0:
+            alkuluku = False
+    if alkuluku:
+        print("Lukusi on alkuluku")
+    else:
+        print("Lukusi ei ole alkuluku")
+else:
+    print("Lukusi ei ole alkuluku")
+
+#Tehtävä 5.4
+kaupungit = []
+for i in range(0,5):
+    kaupungit.append(input("Anna jokin kaupunki"))
+for i in range(0,len(kaupungit)):
+    print(kaupungit[i])
+    i += 1
